18f47c3b4e589db892c60b25864e9182a8fe449c
Shubham-Pangale-au28 <pangaleshubham10@gmail.com>
2023-02-28 10:45:29 +0530

    Fisrt Project Commited.....

---
+++ JavaFisrtProject/src/com/first/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.first;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int n1 = 0, n2 = 1, n3, i, count = 10;
+		
+
+	}
+
+}
